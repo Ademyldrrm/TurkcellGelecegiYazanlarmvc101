38b0b47b1fb01e2d45ad2d88248aa0dc585194e5
Adem Yıldırım <yldrmadem52@gmail.com>
2023-02-03 16:44:04 +0300

    Project complated

---
+++ MyAspNetCoreApp.Web/Controllers/OrnekConroller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyAspNetCoreApp.Web.Controllers
+{
+    public class OrnekConroller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
